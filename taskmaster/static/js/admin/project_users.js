126693781a80c7768cb2d0b3d9406c177d8dd571
Jonathan Munz <jpmunz@gmail.com>
2014-09-07 19:29:25 -0400

    Project users backbone view

--- taskmaster/static/js/admin/project_users.js
+++ taskmaster/static/js/admin/project_users.js
@@ -1,11 +1,39 @@
-$(function() {
-  $('.user-role-container.permitted').clickToEdit({
-    inputType: 'select',
-    choices: STATE.user.lte_roles,
-    success: function($editable) {
-      var userId = $editable.parents('tr').data('user-id'),
-          role = $editable.val();
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'text!templates/_project_users.html',
+  'click_to_edit'
+], function($, _, Backbone, ProjectUsersTemplate) {
 
+  var ProjectUsersView = Backbone.View.extend({
+    template: _.template(ProjectUsersTemplate),
+    events: {
+      'submit .invite-user': 'inviteUser',
+      'click .cancel-invite': 'cancelInvite',
+      'click .kick-user': 'kickUser'
+    },
+
+    render: function() {
+      this.$el.html(this.template({
+        STATE: STATE
+      }));
+
+      this.$('.user-role-container.permitted').clickToEdit({
+        inputType: 'select',
+        choices: STATE.user.lte_roles,
+        success: function($editable) {
+          var userId = $editable.parents('tr').data('user-id'),
+              role = $editable.val();
+
+          this.updateRole(userId, role);
+        }.bind(this)
+      });
+
+      return this;
+    },
+
+    updateRole: function(userId, role) {
       $.ajax({
         type: 'PUT',
         url: '/org/' + STATE.org.id + '/user/' + userId + '/role/' + role + '/',
@@ -13,45 +41,47 @@ $(function() {
           window.location.reload();
         }
       });
+    },
+
+    inviteUser: function(event) {
+      event.stopPropagation();
+      event.preventDefault();
+
+      var email = $('.invite-user-email').val(),
+          role = $('.invite-user-role').val();
+
+      $.ajax({
+        type: 'POST',
+        url: '/org/' + STATE.org.id + '/user/' + email + '/',
+        data: {
+          role: role
+        },
+        success: function() {
+          window.location.reload();
+        }
+      });
+    },
+
+    cancelInvite: function(event) {
+      var email = $(event.target).parents('tr').find('.pending-email').text();
+      this.deleteUser(email);
+    },
+
+    kickUser: function(event) {
+      var userId = $(event.target).parents('tr').data('user-id');
+      this.deleteUser(userId);
+    },
+
+    deleteUser: function(username) {
+      $.ajax({
+        type: 'DELETE',
+        url: '/org/' + STATE.org.id + '/user/' + username + '/',
+        success: function() {
+          window.location.reload();
+        }
+      });
     }
   });
 
-  $('.invite-user').on('submit', function(event) {
-    event.stopPropagation();
-    event.preventDefault();
-
-    var email = $('.invite-user-email').val(),
-        role = $('.invite-user-role').val();
-
-    $.ajax({
-      type: 'POST',
-      url: '/org/' + STATE.org.id + '/user/' + email + '/',
-      data: {
-        role: role
-      },
-      success: function() {
-        window.location.reload();
-      }
-    });
-  });
-
-  function deleteUser(username) {
-    $.ajax({
-      type: 'DELETE',
-      url: '/org/' + STATE.org.id + '/user/' + username + '/',
-      success: function() {
-        window.location.reload();
-      }
-    });
-  }
-
-  $('.cancel-invite').click(function(event) {
-    var email = $(this).parents('tr').find('.pending-email').text();
-    deleteUser(email);
-  });
-
-  $('.kick-user').click(function(event) {
-    var userId = $(this).parents('tr').data('user-id');
-    deleteUser(userId);
-  });
+  return ProjectUsersView;
 });
